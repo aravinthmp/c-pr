4ea45f768bd213dd28a0a41d959de8b23ebe3c35
balajidhoni <35687870+balajidhoni@users.noreply.github.com>
2018-02-05 13:45:42 +0530

    Create k.c

---
+++ k.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int m,reversed integer=0,remainder,original integer;
+printf("enter a integer:");
+scanf("%d",&m);
+originalInteger=m;
+while(m!=0)
+{
+remainder=m%10;
+m/10;
+}
+if(originalInteger==reverseInteger)
+printf(("%d is a palindrome",originalInteger);
+else
+printf("%d is not a palindrome",originalInteger);
+return 0;
+}
