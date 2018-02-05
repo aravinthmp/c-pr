df724a13fca337232ee45a9201878d732792cf03
balajidhoni <35687870+balajidhoni@users.noreply.github.com>
2018-02-05 13:59:31 +0530

    Create balaji7.c

---
+++ balaji7.c
@@ -0,0 +1,26 @@
+
+#include<stdio.h>
+
+void main()
+{ 
+    int j,i,r,t;
+    scanf("%d",&n);
+    scanf("%d",&r);
+    j++;
+    while(j<r)
+    {   t=0;
+        for(i=2;i<j/2;i++)
+        {
+            if(j%i==0)
+            {
+                t=1;
+                break;
+            }
+        }
+        if(t==0)
+        printf("%d",j);
+        
+        j++;
+    }
+   
+}
