75952808408e5963cf00df1cb1e94541e3c845fc
balajidhoni <35687870+balajidhoni@users.noreply.github.com>
2018-02-07 14:15:01 +0530

    Create karthik balaji19.c

---
+++ karthik balaji19.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[5]= {5,4,6,8,9};
+int *p=&a[0];
+int i;
+clrscr();
+for(i=0; i<5; i++)
+printf("\nArray[%d] is %d ",i,*(p+i));
+for(i=0; i<5; i++)
+printf("\n %d at %u ",*(p+i),(p+i));
+getch();
+}
