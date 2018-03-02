a3c024e915b08380b8406c12fd780f0a3309673d
balajidhoni <35687870+balajidhoni@users.noreply.github.com>
2018-03-02 15:56:58 +0530

    Update balaji48.c

--- balaji48.c
+++ balaji48.c
@@ -4,9 +4,9 @@
 int main(void) 
 {
 	int n1,n2,diff;
-	printf("enter the number1:");
+	printf("enter a number1:");
 	scanf("%d",&n1);
-	printf("enter the number2:");
+	printf("enter a number2:");
 	scanf("%d",&n2);
 	diff=n1-n2;
 	if(diff%2==0)
