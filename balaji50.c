42532f8b4fcf2eeb032f44d24bfc5e64bc4f8728
balajidhoni <35687870+balajidhoni@users.noreply.github.com>
2018-03-05 13:11:24 +0530

    Update balaji50.c

--- balaji50.c
+++ balaji50.c
@@ -2,23 +2,23 @@
 #include<string.h>
 int main(void) 
 {
-	char str[50],temp[10];
-	int i,j,n,k;
-	printf("enter a string:");
-	scanf("%s",str);
-	n=strlen(str);
-	while(i<j)
-	{
-		for(i=0;i<n;i++)
-		{
-			for(j=n;j>=1;j--)
-			{
-				temp[k]=str[i];
-				str[i]=str[j];
-				str[j]=str[i];
-			}
-		}
-	}
-	printf("\nyes");
-	return 0;
+char str[50],temp[10];
+int i,j,n,k;
+printf("enter a string:");
+scanf("%s",str);
+n=strlen(str);
+while(i<j)
+{
+for(i=0;i<n;i++)
+{
+for(j=n;j>=1;j--)
+{
+temp[k]=str[i];
+str[i]=str[j];
+str[j]=str[i];
+}
+}
+}
+printf("\nyes");
+return 0;
 }
