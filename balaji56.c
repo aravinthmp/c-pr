b05edefa6fb6454328468cd5ab301254c04a94c4
balajidhoni <35687870+balajidhoni@users.noreply.github.com>
2018-03-05 13:39:19 +0530

    Update balaji56.c

--- balaji56.c
+++ balaji56.c
@@ -2,15 +2,15 @@
 
 int main(void) 
 {
-	int n,i;
-	printf("enter the number:");
-	scanf("%d",&n);
-	for(i=1;i<=n;i++)
-	{
-		if(n%i==0)
-		{
-			printf("%d ",i);
-		}
-	}
-	return 0;
+int n,i;
+printf("enter the number:");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+if(n%i==0)
+{
+printf("%d ",i);
+}
+}
+return 0;
 }
