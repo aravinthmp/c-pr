27124757dd14acc2fd69a707b0dcecdddc5068c3
balajidhoni <35687870+balajidhoni@users.noreply.github.com>
2018-03-07 14:56:00 +0530

    Update balaji65.c

--- balaji65.c
+++ balaji65.c
@@ -2,25 +2,25 @@
 #include<string.h>
 int main(void) 
 {
-	char str[50];
-	int i,flag=0;
-	printf("\nenter the string:");
-	scanf("%s",str);
-	for(i=0;str[i]!='\0';i++)
-	{
-		if(str[i]==str[i+1])
-		{
-			flag=1;
-			break;
-		}
-	}
-	if(flag==1)
-	{
-		printf("\nno");
-	}
-	else
-	{
-		printf("\nyes");
-	}
-	return 0;
+char str[50];
+int i,flag=0;
+printf("\nenter a string:");
+scanf("%s",str);
+for(i=0;str[i]!='\0';i++)
+{
+if(str[i]==str[i+1])
+{
+flag=1;
+break;
+}
+}
+if(flag==1)
+{
+printf("\nno");
+}
+else
+{
+printf("\nyes");
+}
+return 0;
 }
