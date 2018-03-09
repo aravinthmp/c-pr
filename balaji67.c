5f2e1dc62db7d0e069a202a16cf5a1f5ee412b10
balajidhoni <35687870+balajidhoni@users.noreply.github.com>
2018-03-09 15:50:14 +0530

    Create balaji67.c

---
+++ balaji67.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(void) 
+{
+int x,y,remainder,lcm,gcd,a,b;
+printf("\n enter a first number:");
+scanf("%d",&x);
+printf("\n enter a second number:");
+scanf("%d",&y);
+a=x;
+b=y;
+do
+{
+remainder=x%y;
+if(remainder==0)
+break;
+x=y;
+y=remainder;
+}while(remainder!=0);
+gcd=y;
+lcm=(a*b)/gcd;
+printf("\n enter a given number is: %d",lcm);
+return 0;
+}
